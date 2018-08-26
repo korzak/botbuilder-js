e1540af63c5b9544637139cbc01e38daa6c87a0b
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-08-26 15:59:05 -0700

    clean up echo bot

--- samples/2.echobot-with-counter/dialogs/mainDialog/index.js
+++ samples/2.echobot-with-counter/dialogs/mainDialog/index.js
@@ -1,12 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const mainState = require('./mainState');
- 
+// Turn counter property
+const TURN_COUNTER = 'turnCounter';
+
 class MainDialog {
 
     constructor (conversationState) {
-        this.state = new mainState(conversationState);
+        // creates a new state accessor property.see https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors 
+        this.countProperty = conversationState.createProperty(TURN_COUNTER);
     }
 
     // handle the OnTurn
@@ -14,13 +16,12 @@ class MainDialog {
     async onTurn(context) {
         // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
         if (context.activity.type === 'message') {
-            
             // read from state.
-            const count = await this.state.get(context);
+            let count = await this.countProperty.get(context);
             count = count === undefined ? 0 : count;
             await context.sendActivity(`${count}: You said "${context.activity.text}"`);
             // increment and set turn counter.
-            this.state.set(context, ++count);
+            this.countProperty.set(context, ++count);
         }
         else {
             await context.sendActivity(`[${context.activity.type} event detected]`);
